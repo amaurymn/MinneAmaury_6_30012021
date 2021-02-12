880225f3a9999537c7812f6cc116053ac45a42cc
amaurymn <53151363+amaurymn@users.noreply.github.com>
2021-02-12 15:50:17 +0100

    #3 : Add bootstrap/fontawesome/jquery

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-// start the Stimulus application
-import './bootstrap';
+// Javascripts
+import jquery from 'jquery';
+import bootstrap from 'bootstrap';
 
+// start the Stimulus application
+//import './bootstrap';
