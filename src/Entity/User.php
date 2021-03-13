7a0de1b5a984d05baf7d2a96f3178d0fa18ad303
amaurymn <53151363+amaurymn@users.noreply.github.com>
2021-03-13 01:02:34 +0100

    09: remove constraints from entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,8 +10,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
- * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
 {
@@ -70,7 +68,7 @@ class User implements UserInterface
         return $this->email;
     }
 
-    public function setEmail(string $email): self
+    public function setEmail(?string $email): self
     {
         $this->email = $email;
 
